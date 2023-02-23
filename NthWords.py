909f453f3d7360e5deee59307af48dc0d725cb66
Jacob Weston-Davis <u7003724@anu.edu.au>
2023-02-23 16:55:22 +1100

    revamped to be a generic tool with command line input

--- NthWords.py
+++ NthWords.py
@@ -1,19 +1,19 @@
 import string
 import re
 import sys
+import argparse
 
 def write_data(oup, loc):
     f = open(loc, 'w', encoding='utf-8')
     f.write(oup)
     f.close()
     
-def thousandthWords(path):
+def nthWords(path, n):
     # Reading in the chapter
     f = open(path, 'r', encoding='utf8')
     contents = f.read()
-
+    f.close()
     # Cleaning up the string to only include the chapter
-    # print(len(contents.split("<!--chapter content-->", 1)))
     mc = contents.split("<!--chapter content-->", 1)[1]
     mc = mc.strip()
     mc = mc.rsplit("<!--/chapter content-->", 1)[0]
@@ -23,20 +23,20 @@ def thousandthWords(path):
     # https://github.com/otwcode/otwarchive/blob/master/lib/word_counter.rb
     mc = re.sub("[--]", "—", mc)
     mc = re.sub("['’‘-]", "", mc)
-    # print(type(mc))
-    # write_data(mc,'contents.txt')
     result = len(re.findall(r'\w+', mc))
     poggers = re.findall(r'\w+', mc)
-    # print(poggers[:1000])
-    ous = ' '.join(poggers[::1000])
+    ous = ' '.join(poggers[::n])
     write_data(ous,'oup.txt')
-    # print(result)
-    # print(mc)
-    
-    f.close()
+
     return result
 
-
-thousandthWords('Run at the Cup.html')
+parser = argparse.ArgumentParser()
+parser.add_argument('file', type=str, help='The html file to process')
+parser.add_argument('n', type=int, help='How many words between each word in the output. Must be >=1')
+args = parser.parse_args()
+if (args.n < 1):
+    print("n must be a positive integer")
+else:
+    nthWords(args.file, args.n)
 
 
