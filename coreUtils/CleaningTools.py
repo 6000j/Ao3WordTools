3e7f3856d73468f9795aba1032c049ffa8bc236e
Jacob Weston-Davis <u7003724@anu.edu.au>
2023-04-14 21:11:03 +1000

    Even more accurate word count. This is where I am going to stop, as this code would pass every one of the English-language tests in the ao3 repository for their word counter (i.e. you could replace the current one with this and it would be accepted)

--- coreUtils/CleaningTools.py
+++ coreUtils/CleaningTools.py
@@ -7,13 +7,12 @@ def clean_html(inu):
     '''Cleans up all html formatting in the input string, returns only words with no punctuation'''
     chapter = inu
     # Cleaning up html tags
-    chapter = re.sub("<(.*?)>", "", chapter)
+    chapter = re.sub("<(.*?)>", " ", chapter)
     # Cleaning up HTML Entities
-    chapter = re.sub("&(.+?);", "", chapter)
-    # chapter = re.sub("&#(.+?);", "", chapter)
+    chapter = re.sub("&(.+?);", " ", chapter)
     # This part is an approximation of the Ao3 word counter, which can be found here
     # https://github.com/otwcode/otwarchive/blob/master/lib/word_counter.rb 
-    chapter = re.sub("[--]", "", chapter)
-    chapter = re.sub("['’‘\-—]", "", chapter)
+    chapter = re.sub("--", "—", chapter)
+    chapter = re.sub("['’‘-]", "", chapter)
 
     return chapter
--- coreUtils/WordCountTools.py
+++ coreUtils/WordCountTools.py
@@ -1,4 +1,5 @@
 import re
+import coreUtils.CleaningTools
 
 # A bunch of misc useful wordcount-related functions
 
@@ -10,3 +11,8 @@ def words_list(inp):
 def simplified_word_count(inp):
     '''Returns the word count of the input string, assuming it has already been correctly cleaned'''
     return len(words_list(inp))
+
+
+def cleaned_word_count(inp):
+    '''Returns the word count of the input string, along with cleaning it first'''
+    return simplified_word_count(coreUtils.CleaningTools.clean_html(inp))
